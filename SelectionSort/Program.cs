f6241e5b0957aa30a919be1682a95cbf0220c9bc
Kaan Tosun <tr.kaantsn@gmail.com>
2023-03-07 20:01:28 +0300

    Add project files.

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = { 1, 9, 0, 7, -6, -8 };
+            int temp = 0;
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                for (int j = i; j < dizi.Length; j++)
+                {
+                    if (dizi[i] > dizi[j])
+                    {
+                        temp = dizi[j];
+                        dizi[j] = dizi[i];
+                        dizi[i] = temp;
+                    }
+                }
+            }
+            foreach (int i in dizi)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
